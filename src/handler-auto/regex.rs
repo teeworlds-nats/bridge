8f3ef5a07b2cf3c1e52297e6e029b157342cdaa3
ByFox213 <byfox213@gmail.com>
2025-02-13 09:19:24 +0500

    fix and fix

--- src/handler-auto/regex.rs
+++ src/handler-auto/regex.rs
@@ -7,9 +7,6 @@ pub static DEFAULT_REGEX: Lazy<Vec<RegexData>> = Lazy::new(|| {
             "DDNet".to_string(),
             r"^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) (I|E) ([a-z]+): (.*)".to_string(),
         ),
-        RegexData::new(
-            "Teeworlds".to_string(),
-            r"^\[(\w+)\]: (.*)".to_string(),
-        ),
+        RegexData::new("Teeworlds".to_string(), r"^\[(\w+)\]: (.*)".to_string()),
     ]
 });
